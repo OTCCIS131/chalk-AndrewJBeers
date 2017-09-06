acc20eeacdaa182ab9e7c78de18eba0bd1054f94
BEERS <ajbeers89@gmail.com>
2017-09-06 13:18:58 -0500

    rgb

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log(chalk.blue('Happy birthday to you, ') + chalk.red('cha, ')+ chalk.ye
 console.log(chalk.greenBright.bold.bgYellowBright('Happy birthday to you, ') + chalk.cyanBright('cha, ')+ chalk.white('cha, ')+ chalk.bgMagenta('cha.'));
 console.log(chalk.blue('Happy birthday Dear, ') + chalk.bold.green.underline('A')+ chalk.bold.yellow.underline('D')+ chalk.bold.underline.cyanBright('A')+ chalk.bold.underline.redBright('M.'));
 console.log(chalk.italic.magentaBright.bgRedBright('Happy birthday to you, ') + chalk.magenta('cha, ')+ chalk.green('cha, ')+ chalk.redBright('cha.'));
+console.log(chalk.bold.rgb(153, 0, 204)('Hello!'));
+ 
